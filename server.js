b1bc4b78086f8a287d267311db71c8634bb6e831
mostafa.mekkawy3@gmail.com <Mostafa.mekkawy3@gmail.com>
2020-10-24 16:28:19 +0200

    make it better

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const morgan= require('morgan');
 const path=require('path');
 const connectDB=require('./config/db')
 
-dotenv.config({path:"./config/config.env"});
+dotenv.config({path:".env"});
 const transactions= require('./routes/transactions');
 const app= express();
 connectDB();
@@ -19,7 +19,7 @@ if(process.env.NODE_ENV==="development"){
 }
 app.use('/api/v1/transactions', transactions);
 if(process.env.NODE_ENV=== 'production'){
-	app.use(express.static('client/build'));
+	app.use(express.static(path.join(__dirname,'client/build')));
 	app.get('*',(req,res)=> res.sendFile(path.resolve(__dirname,'client','build','index.html')));
 
 }
