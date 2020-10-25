9bddc9b14a24c100b5ddc9eb0d18afa4849b4103
mostafa.mekkawy3@gmail.com <mostafa.mekkawy3@gmail.com>
2020-10-25 12:31:57 +0100

    '

---
+++ client/build/precache-manifest.b247a55bedbca14ff08515eeaa3312a6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9074450d82cf09a6d0a824ec982a486",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ac1478657571c75a13fe",
+    "url": "/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "7d97565a6f0d83efdc6c",
+    "url": "/static/js/2.0f2b02c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0f2b02c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac1478657571c75a13fe",
+    "url": "/static/js/main.87ae7aca.chunk.js"
+  },
+  {
+    "revision": "1eede93b43bca28572ca",
+    "url": "/static/js/runtime-main.399baf1c.js"
+  }
+]);
